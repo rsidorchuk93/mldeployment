9ee446ece28f89efbcda0d7048697fccd0f7fe35
Roman Sidorchuk <romansidorchuk@gmail.com>
2023-03-18 18:12:09 -0400

    Model trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,12 +1,12 @@
 import sys
 from dataclasses import dataclass
 
-import numpy as np
+import numpy as np 
 import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
 
 from src.exception import CustomException
 from src.logger import logging
@@ -16,7 +16,7 @@ from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path = os.path.join('artifacts',"preprocessor.pkl")
+    preprocessor_obj_file_path=os.path.join('artifacts',"proprocessor.pkl")
 
 class DataTransformation:
     def __init__(self):
@@ -24,7 +24,7 @@ class DataTransformation:
 
     def get_data_transformer_object(self):
         '''
-        This function is responsible for data transformation
+        This function si responsible for data trnasformation
         
         '''
         try:
@@ -73,39 +73,43 @@ class DataTransformation:
         except Exception as e:
             raise CustomException(e,sys)
         
-    def initiate_data_transformation(self, train_path, test_path):
+    def initiate_data_transformation(self,train_path,test_path):
 
         try:
-            train_df = pd.read_csv(train_path)
-            test_df = pd.read_csv(test_path)
+            train_df=pd.read_csv(train_path)
+            test_df=pd.read_csv(test_path)
 
             logging.info("Read train and test data completed")
-            logging.info("Obtaining preprocessor object")
 
-            preprocessing_obj = self.get_data_transformer_object()
+            logging.info("Obtaining preprocessing object")
 
-            target_column_name = "math_score"
+            preprocessing_obj=self.get_data_transformer_object()
+
+            target_column_name="math_score"
             numerical_columns = ["writing_score", "reading_score"]
 
-            input_feature_train_df = train_df.drop(target_column_name, axis=1)
-            target_feature_train_df = train_df[target_column_name]
+            input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
+            target_feature_train_df=train_df[target_column_name]
 
-            input_feature_test_df = test_df.drop(target_column_name, axis=1)
-            target_feature_test_df = test_df[target_column_name]
+            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
+            target_feature_test_df=test_df[target_column_name]
 
-            input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
-            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
-            
+            logging.info(
+                f"Applying preprocessing object on training dataframe and testing dataframe."
+            )
+
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
+
             train_arr = np.c_[
                 input_feature_train_arr, np.array(target_feature_train_df)
-                ]
-            test_arr = np.c_[
-                input_feature_test_arr, np.array(target_feature_test_df)
-                ]
+            ]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
 
             logging.info(f"Saved preprocessing object.")
 
             save_object(
+
                 file_path=self.data_transformation_config.preprocessor_obj_file_path,
                 obj=preprocessing_obj
 
@@ -115,8 +119,6 @@ class DataTransformation:
                 train_arr,
                 test_arr,
                 self.data_transformation_config.preprocessor_obj_file_path,
-            )                      
-
+            )
         except Exception as e:
-            raise CustomException(e, sys)
-        
+            raise CustomException(e,sys)
